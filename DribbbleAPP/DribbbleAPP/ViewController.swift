ae50803c0a318011dc6ac452d18ef1273e1d3626
shumv <shumkovitaliy@gmail.com>
2016-10-11 16:25:28 +0300

    my first commit ^ )

---
+++ DribbbleAPP/DribbbleAPP/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  DribbbleAPP
+//
+//  Created by Виталий Шумко on 04.10.16.
+//  Copyright © 2016 Виталий Шумко. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://dribbble.com/oauth/authorize",
+                          parameters: ["client_id": "d15429c5cf672e0182f610f5c967ca58fdecd2f4d086e5f61ce7908a3a41e7f6",
+                                       "redirect_uri" : "https://dribbble.com/",
+                                       "scope" : "public+write+comment"
+                                       ]).responseJSON() {
+            response in
+            print("Response.result!!!!!")
+            print(response.request)
+            print(response.response)
+            print(response.data)
+            print(response.result)
+                                    
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DribbbleAPP/Pods/Target Support Files/Pods-DribbbleAPP/Pods-DribbbleAPP-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DribbbleAPPVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DribbbleAPPVersionString[];
+
