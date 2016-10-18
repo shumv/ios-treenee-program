86ca1a123ad3c20d6ae9e126de0bd3933f1adee7
shumv <shumkovitaliy@gmail.com>
2016-10-18 03:36:22 +0300

    changes

---
+++ DribbbleAPP/DribbbleAPP/DribbbleAPI.swift
@@ -0,0 +1,33 @@
+//
+//  DribbbleAPI.swift
+//  DribbbleAPP
+//
+//  Created by Виталий Шумко on 18.10.16.
+//  Copyright © 2016 Виталий Шумко. All rights reserved.
+//
+
+import Foundation
+
+class DribbbleAPI {
+    
+    let accessToken = "e924c23f3e6bb64176c28b23bfc7da19adcd296227c6fc5971230183735dc00c"
+    let accessClientID = "d15429c5cf672e0182f610f5c967ca58fdecd2f4d086e5f61ce7908a3a41e7f6"
+    func loadShots(completion: ((AnyObject) -> Void)!) {
+        //var urlString = "https://api.dribbble.com/v1/shots?access_token=" + accessToken
+        //var urlString = "https://api.dribbble.com/v1/shots?access_token=" + accessToken
+        var urlString = "https://dribbble.com/oauth/authorize?client_id=" + accessClientID + "&redirect_uri=" + "https://api.dribbble.com/v1/shots"
+        let session = URLSession.shared
+        let shotsUrl = URL(string: urlString)
+        
+       var task = session.dataTask(with: shotsUrl!) { (data, response, error) in
+        
+        if error != nil{
+                print(error!.localizedDescription)
+            }else{
+            
+            }
+        }
+        
+        task.resume()
+    }
+}
--- DribbbleAPP/DribbbleAPP/ViewController.swift
+++ DribbbleAPP/DribbbleAPP/ViewController.swift
@@ -13,20 +13,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let api = DribbbleAPI()
+        api.loadShots(completion: nil)
         // Do any additional setup after loading the view, typically from a nib.
-        Alamofire.request("https://dribbble.com/oauth/authorize",
-                          parameters: ["client_id": "d15429c5cf672e0182f610f5c967ca58fdecd2f4d086e5f61ce7908a3a41e7f6",
-                                       "redirect_uri" : "https://dribbble.com/",
-                                       "scope" : "public+write+comment"
-                                       ]).responseJSON() {
-            response in
-            print("Response.result!!!!!")
-            print(response.request)
-            print(response.response)
-            print(response.data)
-            print(response.result)
-                                    
-        }
+//        Alamofire.request("https://dribbble.com/oauth/authorize",
+//                          parameters: ["client_id": "d15429c5cf672e0182f610f5c967ca58fdecd2f4d086e5f61ce7908a3a41e7f6",
+//                                       "redirect_uri" : "https://dribbble.com/",
+//                                       "scope" : "public+write+comment"
+//                                       ]).responseJSON() {
+//            response in
+//            print("Response.result!!!!!")
+//            print(response.request)
+//            print(response.response)
+//            print(response.data)
+//            print(response.result)
+//                                    
+//        }
     }
 
     override func didReceiveMemoryWarning() {
