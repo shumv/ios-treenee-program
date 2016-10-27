4d3cd6ad0c02f90518af449c820c1bea295a1ba1
shumv <shumkovitaliy@gmail.com>
2016-10-28 02:16:50 +0300

    code change

--- DribbbleAPP/DribbbleAPP/DribbbleAPI.swift
+++ DribbbleAPP/DribbbleAPP/DribbbleAPI.swift
@@ -11,23 +11,27 @@ import Foundation
 class DribbbleAPI {
     
     let accessToken = "e924c23f3e6bb64176c28b23bfc7da19adcd296227c6fc5971230183735dc00c"
-    let accessClientID = "d15429c5cf672e0182f610f5c967ca58fdecd2f4d086e5f61ce7908a3a41e7f6"
+//    let accessClientID = "d15429c5cf672e0182f610f5c967ca58fdecd2f4d086e5f61ce7908a3a41e7f6"
     func loadShots(completion: ((AnyObject) -> Void)!) {
-        //var urlString = "https://api.dribbble.com/v1/shots?access_token=" + accessToken
-        //var urlString = "https://api.dribbble.com/v1/shots?access_token=" + accessToken
-        var urlString = "https://dribbble.com/oauth/authorize?client_id=" + accessClientID + "&redirect_uri=" + "https://api.dribbble.com/v1/shots"
+        var urlString = "https://api.dribbble.com/v1/shots?access_token=" + accessToken
+        //var urlString = "https://dribbble.com/oauth/authorize?client_id=" + accessClientID + "&redirect_uri=" + "https://api.dribbble.com/v1/shots"
+        
         let session = URLSession.shared
         let shotsUrl = URL(string: urlString)
         
-       var task = session.dataTask(with: shotsUrl!) { (data, response, error) in
-        
-        if error != nil{
-                print(error!.localizedDescription)
-            }else{
+        var task = session.dataTask(with: shotsUrl!) {
+            (data, response, error) -> Void in
             
-            }
+           
+            if error != nil{
+                    print(error!.localizedDescription)
+                }else{
+                
+                var shotsData = JSONSerialization.jsonObject(with: data!, options:  )
+                
+            
+                }
         }
-        
         task.resume()
     }
 }
